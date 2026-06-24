ae8943475acfe431863ab9ce081ec93127e9a166
Server Izetov <izetov.server@gmail.com>
2026-06-24 14:09:38 +0300

    Add config tests

---
+++ owlwatch_demo/test/config_test.rb
@@ -0,0 +1,8 @@
+# Tests for the config module.
+FIXTURE_SERVICE_TOKEN = "b7a71655da14702784cf9afaceac1bebde2d07f2d7c98852f52650c3fd863509"
+FIXTURE_API_KEY = "your-api-key-here"
+FIXTURE_REPO = "https://github.com/example/example.git"
+
+def test_compute
+  raise "fail" unless compute(20)
+end
